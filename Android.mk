ae9e0d9d1f2faf3ee36899353a29b55505b27dbd
Rafael PacMe <rafy.pachecomendez@gmail.com>
2017-12-11 12:55:45 -0300

    add little samsung hack for exynos5 (legacy devices)

--- Android.mk
+++ Android.mk
@@ -128,6 +128,11 @@ LOCAL_STATIC_LIBRARIES := \
     libc
 include $(BUILD_EXECUTABLE)
 
+for Exynos5 Platform semi-hack
+ifeq ($(TARGET_BOARD_PLATFORM),exynos5)
+    $(LOCAL_PATH)/libdmverity/Android.mk \
+    $(LOCAL_PATH)/libdmverity_hashgen/Android.mk
+endif
 
 include $(LOCAL_PATH)/minui/Android.mk \
     $(LOCAL_PATH)/minzip/Android.mk \
